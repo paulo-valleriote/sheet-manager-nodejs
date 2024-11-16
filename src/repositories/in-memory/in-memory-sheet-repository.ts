e2a2ac99289ff2257e5ceeaf6439bf85e719b763
paulo-mv <paulo@infratoken.app>
2024-11-16 15:22:03 -0300

    feat: in memory sheet repository implementation

--- src/repositories/in-memory/in-memory-sheet-repository.ts
+++ src/repositories/in-memory/in-memory-sheet-repository.ts
@@ -1,34 +1,54 @@
-import { ISheet } from "@/domain/entities/Sheet";
-import type { ISheetsRepository } from "../sheets-repository";
+import { randomUUID } from 'node:crypto';
+import type { ISheet } from '@/domain/entities/Sheet';
+import type { ISheetsRepository } from '../sheets-repository';
+import type { ICreateSheetParams, IDeleteSheetParams, IGetSheetParams, IGetSheetResponse, IListSheetsParams, IListSheetsResponse, IUpdateSheetParams } from '../@types/sheets';
 
 export class InMemorySheetRepository implements ISheetsRepository {
   private sheets: ISheet[] = []
 
-  async list(user_id: string): Promise<ISheet[]> {
-    return this.sheets.filter((sheet) => sheet.userId === user_id)
+  async list(data: IListSheetsParams): Promise<IListSheetsResponse> {
+    const sheets = this.sheets.filter((sheet) => sheet.userId === data.userId)
+
+    return {
+      data: sheets
+    }
   }
 
-  async get(sheet_id: string): Promise<ISheet | null> {
-    const sheet = this.sheets.find((sheet) => sheet.id === sheet_id)
+  async get(data: IGetSheetParams): Promise<IGetSheetResponse> {
+    const sheet = this.sheets.find((sheet) => sheet.id === data.sheetId)
 
     if (!sheet) {
-      return null
+      return {
+        data: null
+      }
     }
 
-    return sheet
+    return {
+      data: sheet
+    }
   }
 
-  async create(data: any): Promise<void> {
-    this.sheets.push(data)
+  async create(data: ICreateSheetParams): Promise<void> {
+    this.sheets.push({
+      id: randomUUID(),
+      name: data.name,
+      userId: data.userId,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
   }
 
-  async update(sheet_id: string, data: any): Promise<void> {
-    const sheetIndex = this.sheets.findIndex((sheet) => sheet.id === sheet_id)
-    this.sheets[sheetIndex] = data
+  async update(data: IUpdateSheetParams): Promise<void> {
+    const sheetIndex = this.sheets.findIndex((sheet) => sheet.id === data.sheetId)
+    this.sheets[sheetIndex] = {
+      ...this.sheets[sheetIndex],
+      ...data,
+      updatedAt: new Date()
+    }
 	}
   
-  async delete(sheet_id: string): Promise<void> {
-    const sheetIndex = this.sheets.findIndex((sheet) => sheet.id === sheet_id)
+  async delete(data: IDeleteSheetParams): Promise<void> {
+    const sheetIndex = this.sheets.findIndex((sheet) => sheet.id === data.sheetId)
     this.sheets.splice(sheetIndex, 1)
   }
 }
