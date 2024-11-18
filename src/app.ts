c562bd8ecb8d946cce3ef24c0b06c95b6423ae36
paulo-mv <paulo@infratoken.app>
2024-11-18 19:12:45 -0300

    feat: add global handler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify'
 import { ENV } from '@env'
+import { ZodError } from 'zod'
 
 const app = Fastify({
 	logger: ENV.NODE_ENV === 'dev',
@@ -7,6 +8,19 @@ const app = Fastify({
 
 app.get('/', (request, response) => {
 	return 'Hello World!'
+})
+
+// Global error handler
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (ENV.NODE_ENV !== 'prod') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
 })
 
 export default app
