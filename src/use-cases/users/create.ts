d6c09bb6804bfd8fde00a2f0860c31e2551d8454
paulo-mv <paulo@infratoken.app>
2024-11-18 19:37:29 -0300

    refactor: add hashing to create user use-case

--- src/use-cases/users/create.ts
+++ src/use-cases/users/create.ts
@@ -1,4 +1,5 @@
-import type { IUsersRepository } from "@/repositories/users-repository";
+import type { ICryptHandler } from '@/lib/@types/crypt'
+import type { IUsersRepository } from '@/repositories/users-repository'
 
 interface ICreateUserUseCaseParams {
 	email: string
@@ -6,10 +7,13 @@ interface ICreateUserUseCaseParams {
 }
 
 export class CreateUserUseCase {
-	constructor(private userRepository: IUsersRepository) {}
+	constructor(
+		private userRepository: IUsersRepository,
+		private cryptHandler: ICryptHandler
+	) {}
 
 	async execute(data: ICreateUserUseCaseParams) {
-		const passwordHash = ''
+		const passwordHash = await this.cryptHandler.hash(data.password)
 
 		await this.userRepository.create({
 			email: data.email,
