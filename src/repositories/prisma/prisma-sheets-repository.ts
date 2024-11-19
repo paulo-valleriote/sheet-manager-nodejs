04c86771be6276951fdefbdce15280931560e513
paulo-mv <paulo@infratoken.app>
2024-11-19 10:26:56 -0300

    feat: add prisma repositories

---
+++ src/repositories/prisma/prisma-sheets-repository.ts
@@ -0,0 +1,56 @@
+import { prisma } from '@/lib/prisma'
+import type {
+  ICreateSheetParams,
+  IDeleteSheetParams,
+  IGetSheetParams,
+  IGetSheetResponse,
+  IListSheetsParams,
+  IListSheetsResponse,
+  IUpdateSheetParams,
+} from '../@types/sheets'
+import type { ISheetsRepository } from '../sheets-repository'
+
+export class PrismaSheetsRepository implements ISheetsRepository {
+  async create(data: ICreateSheetParams): Promise<void> {
+    await prisma.sheet.create({
+      data,
+    })
+  }
+
+  async delete(data: IDeleteSheetParams): Promise<void> {
+    await prisma.sheet.delete({
+      where: {
+        id: data.sheetId,
+      },
+    })
+  }
+
+  async get(data: IGetSheetParams): Promise<IGetSheetResponse> {
+    const sheet = await prisma.sheet.findUnique({
+      where: {
+        id: data.sheetId,
+      },
+    })
+
+    return { data: sheet ?? null }
+  }
+
+  async list(data: IListSheetsParams): Promise<IListSheetsResponse> {
+    const sheets = await prisma.sheet.findMany({
+      where: {
+        userId: data.userId,
+      },
+    })
+
+    return { data: sheets }
+  }
+
+  async update(data: IUpdateSheetParams): Promise<void> {
+    await prisma.sheet.update({
+      where: {
+        id: data.sheetId,
+      },
+      data,
+    })
+  }
+}
---
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -0,0 +1,67 @@
+import { prisma } from '@/lib/prisma'
+import type {
+  ICreateUserParams,
+  IDeleteUserParams,
+  IGetUserParams,
+  IGetUserResponse,
+  IListUsersResponse,
+  IUpdateUserParams,
+} from '../@types/users'
+import type { IUsersRepository } from '../users-repository'
+
+export class PrismaUsersRepository implements IUsersRepository {
+  async create(data: ICreateUserParams): Promise<void> {
+    await prisma.user.create({
+      data,
+    })
+  }
+  
+  async delete(data: IDeleteUserParams): Promise<void> {
+    await prisma.user.delete({
+      where: {
+        id: data.userId,
+      },
+    })
+  }
+
+  async getById(data: Pick<IGetUserParams, 'userId'>): Promise<IGetUserResponse> {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: data.userId,
+      },
+    })
+
+    return {
+      data: user ?? null,
+    }
+  }
+
+  async getByEmail(data: Pick<IGetUserParams, 'email'>): Promise<IGetUserResponse> {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: data.email,
+      },
+    })
+
+    return {
+      data: user ?? null,
+    }
+  }
+
+  async list(): Promise<IListUsersResponse> {
+    const users = await prisma.user.findMany()
+
+    return {
+      data: users,
+    }
+  }
+
+  async update(data: IUpdateUserParams): Promise<void> {
+    await prisma.user.update({
+      where: {
+        id: data.userId,
+      },
+      data,
+    })
+  }
+}
