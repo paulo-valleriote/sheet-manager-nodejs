7a62c1936417b00b43426fae7692ecc3c8080636
paulo-valleriote <paulomv29@outlook.com>
2024-11-26 20:30:17 -0300

    refactor: rename class and file

--- src/domain/entities/party-member.ts
+++ src/domain/entities/party-member.ts
@@ -2,13 +2,13 @@ import type { IBaseEntityWithUpdatedAt } from './base-entity'
 import type { IPartyRoles } from './enums/party-roles'
 
 /**
- * Role of party entity
- * @description Role of party application entity, used to link users to parties with a specific role
+ * Party member entity
+ * @description Party member application entity, used to link users to parties with a specific role
  * @property {string} partyId - Party id
  * @property {string} userId - User id
  * @property {IPartyRoles} role - Role of the user in the party
  */
-export interface IRoleOfParty extends IBaseEntityWithUpdatedAt {
+export interface IPartyMember extends IBaseEntityWithUpdatedAt {
   partyId: string
   userId: string
   role: IPartyRoles
