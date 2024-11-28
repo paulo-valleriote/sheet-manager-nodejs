a6894ee6a3b0adff0dcb0caed8cf4a0128123f33
paulo-valleriote <paulomv29@outlook.com>
2024-11-28 19:48:46 -0300

    feat: add update member use case

---
+++ src/use-cases/parties/members/update/update-member.ts
@@ -0,0 +1,24 @@
+import type { IUpdatePartyMemberParams } from '@/repositories/@types/party-members'
+import type { IPartyMembersRepository } from '@/repositories/party-members-repository'
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
+
+export class UpdateMemberUseCase {
+  constructor(private partyMembersRepository: IPartyMembersRepository) {}
+
+  async execute(data: IUpdatePartyMemberParams) {
+    const member = await this.partyMembersRepository.findBy({
+      partyId: data.partyId,
+      userId: data.userId,
+    })
+
+    if (member.data === null) {
+      throw new ResourceNotFoundError()
+    }
+
+    if (member.data.role === 'DUNGEON_MASTER' && data.sheetId) {
+      throw new Error('Dungeon master cannot have a sheet')
+    }
+
+    await this.partyMembersRepository.update(data)
+  }
+}
