b00f7e304f4e62cf9e0871606de3f02f5081d0a1
paulo-valleriote <paulomv29@outlook.com>
2024-11-28 19:47:31 -0300

    refactor: update current sheet logic

--- src/domain/entities/party-member.ts
+++ src/domain/entities/party-member.ts
@@ -6,10 +6,12 @@ import type { IPartyRoles } from './enums/party-roles'
  * @description Party member application entity, used to link users to parties with a specific role
  * @property {string} partyId - Party id
  * @property {string} userId - User id
+ * @property {string} sheetId - Sheet id
  * @property {IPartyRoles} role - Role of the user in the party
  */
 export interface IPartyMember extends IBaseEntity {
   partyId: string
   userId: string
+  sheetId?: string
   role: IPartyRoles
 }
