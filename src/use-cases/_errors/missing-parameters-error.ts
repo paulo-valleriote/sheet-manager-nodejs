cee3eeb56f52219aa1a7c9d1bdfcdd559ac7d777
paulo-valleriote <paulomv29@outlook.com>
2024-12-02 11:06:42 -0300

    feat: add new error type to handle missing parameters

---
+++ src/use-cases/_errors/missing-parameters-error.ts
@@ -0,0 +1,17 @@
+/**
+ * @description Error thrown when a required parameter is missing
+ * @example
+ * throw new MissingParametersError('Party invite id is required') // 'Missing parameters, id is required'
+ * throw new MissingParametersError() // 'Missing parameters'
+ */
+export class MissingParametersError extends Error {
+  constructor(additionalMessage?: string) {
+    const defaultMessage = 'Missing parameters'
+
+    if (additionalMessage !== undefined) {
+      super(`${defaultMessage}, ${additionalMessage}`)
+    } else {
+      super(defaultMessage)
+    }
+  }
+}
